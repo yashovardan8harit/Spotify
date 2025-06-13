51ea2cb425443978e704b6ca79d1f24335bca4c7
yashovardan8harit <yashovardhan.harit2022@vitstudent.ac.in>
2025-06-13 17:20:25 +0530

    adding backend

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -8,7 +8,7 @@ import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'http://localhost:4000'
+export const url = 'https://spotify-gp5f.onrender.com'
 
 const App = () => {
   return (
