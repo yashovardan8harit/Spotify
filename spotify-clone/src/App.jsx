87839d3976f2dfad2d5def5a503d26d5dafc6b44
yashovardan8harit <yashovardhan.harit2022@vitstudent.ac.in>
2025-06-14 10:52:53 +0530

    Build ready

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import Sidebar from './components/sidebar'
+import Sidebar from '../../spotify-admin/src/components/Sidebar' 
 import Player from './components/Player'
 import Display from './components/Display'
 import { playerContext } from './context/playerContext'
